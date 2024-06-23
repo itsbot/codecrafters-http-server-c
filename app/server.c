b3ff1415c2e83904f2675328cb37971f97c4141e
Patrick Iannini <patrickiannini@knights.ucf.edu>
2024-06-23 11:27:54 -0400

    pass 4th stage

--- app/server.c
+++ app/server.c
@@ -9,18 +9,19 @@
 
 #define BUFFER_SIZE 1024
 // Response strings
-char *response_200 = "HTTP/1.1 200 OK\r\n\r\n";
-char *response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+const char *RESPONSE_200 = "HTTP/1.1 200 OK\r\n\r\n";
+const char *RESPONSE_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+char response[BUFFER_SIZE]; // Response buffer
+
+// Function prototypes
+int handleClient(int client_fd);
+int parseRequest(int client_fd, char* request);
+void sendResponse(int client_fd, const char* response);
 
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
  	setbuf(stderr, NULL);
-
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
-
-	// Uncomment this block to pass the first stage
 	
 	int server_fd, client_addr_len;
 	struct sockaddr_in client_addr;
@@ -58,36 +59,59 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	int fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
-	
-	//send(fd, response_200, strlen(response_200), 0);
 
+	// Call handleClient to handle the client request
+	// Return client_fd and request to parseRequest
+	handleClient(client_fd);
+
+	close(server_fd);
+	return 0;
+}
+
+int handleClient(int client_fd) {
 	char request[BUFFER_SIZE];
-	int bytesReceived = recv(fd, request, BUFFER_SIZE - 1, 0);
+	int bytesReceived = recv(client_fd, request, BUFFER_SIZE - 1, 0);
 	if (bytesReceived == -1) {
 		printf("Receive failed: %s\n", strerror(errno));
-		return 1;
+		return -1;
 	}
 	request[bytesReceived] = '\0';
-	printf("Request: %s\n", request);
+	return parseRequest(client_fd, request);
+}
 
+int parseRequest(int client_fd, char* request) {
 	// Extract path from request
 	char *request_path = strtok(request, " ");
 	request_path = strtok(NULL, " ");
+	printf("Request path: %s\n", request_path);
+	//int path_len = strlen(request_path);
 
 	// Check if path is valid
 	if (strcmp(request_path, "/") == 0) {
 		// valid
 		printf("Valid path\n");
-		send(fd, response_200, strlen(response_200), 0);
+		sendResponse(client_fd, RESPONSE_200);
+	}
+	if (strncmp(request_path, "/echo/", 6) == 0) {
+		// echo
+		printf("Echo path\n");
+		// Extract what comes after "/echo/"
+		char *echo_response = request_path + 6;
+		int response_len = strlen(echo_response);
+		sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", response_len, echo_response);
+		sendResponse(client_fd, response);
 	}
 	else {
 		// invalid
 		printf("Invalid path\n");
-		send(fd, response_404, strlen(response_404), 0);
+		sendResponse(client_fd, RESPONSE_404);
 	}
 
-	close(server_fd);
 	return 0;
+}
+
+void sendResponse(int client_fd, const char* response) {
+	send(client_fd, response, strlen(response), 0);
 }
